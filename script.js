c0118ad84154667827e4c29c205ef0075272084d
Ann Pham <annpham001@gmail.com>
2022-06-18 16:42:38 +0930

    wordle updates

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
 
+    //setting global variables 
+    
     const guessedWords = [[]]; //collects are the words in an array, each word will contain an array that contains each letter
     const keys = document.querySelectorAll(".keyboard-row button");
     const word = "DREAM";
     let currentLettersArr = []
 
-    // iterate over each key and add an onclick handler 
+    // for loop - iterate over each key and add an onclick handler 
     for (let i = 0; i < keys.length; i++) {
         keys[i].onclick = ({ target }) => { // target gets our actual letter
             const letter = target.textContent;
             
             const currentRow = guessedWords.length - 1;
 
+
+            // if we are on the 5th letter of the last row (5 as the rows are zero-indexed), enter this if statement: 
             if (guessedWords[currentRow].length === 5) {
+                // if enter button is clicked, submit the wordle 
                 if (letter === 'Enter') {
                     handleSubmitWord()
                 }
-
+                //otherwise do nothing
                return; 
             } 
 
-            if (letter ==='Del') {
-                console.log('Del')
-            }
 
+            if (letter ==='Delete') {
+                handleDeleteLetter() 
+                return;
+            } 
 
-
+        
             updateGuessedWords(letter);
         };
     }  
  
-        //this will tell the number of guessed words so far
+        //this function will tell the number of guessed words so far
     function getCurrentWordArr() { 
         const currentRow = guessedWords.length - 1; //returns the actual array that we're updating 
         const currentLetters = guessedWords[currentRow].length;
@@ -43,7 +49,7 @@
         return guessedWords[currentRow + 1];
     }
 
-     
+    //This function takes the letter (in the guessedWords global variable which will be an array with an array inside of it)   
     function updateGuessedWords(letter) {
         let currentLettersArr = getCurrentWordArr();
 
@@ -63,12 +69,24 @@
         let currentLettersArr = guessedWords[guessedWords.length - 1]
         console.log(currentLettersArr)
 
-        //const currentWord = currentLettersArr.join('') 
 
        let answerLetters = word.split('')
        console.log(answerLetters)
-       
        const row = guessedWords.length - 1 
+
+       const currentWord = currentLettersArr.join('')
+
+       if (currentWord === word) {
+           alert("Winner winner chicken dinner!");
+       } 
+
+       if (guessedWords.length === 6) {
+           alert("Sorry, you lost. The word is DREAM.");
+       }
+
+       //pushes the array to the next row 
+       guessedWords.push([])
+        
        
 
        for (let i = 0; i < currentLettersArr.length; i++) {
@@ -85,13 +103,21 @@
            
        }
 
-       
-
-
 
        }
 
-    
+    //function handleDeleteLetter() {
+
+        //const currentLettersArr = getCurrentWordArr()
+        //const removedLetter = getCurrentWordArr.pop()
+    //}
+
+    //move on to the next line, 
+    //what happens when it moves to the next row 
+    //delete function (what happens something .length) 
+    //win condition 
+
+
 
 
     // seperate function 
